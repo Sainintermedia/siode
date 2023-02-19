fb0958944db3b4cf727d0f211deb1185f2b3d668
nfz123 <akholidin0@gmail.com>
2023-02-20 00:59:20 +0700

    bukukeputusan

---
+++ app/Http/Controllers/Siode/Bukuadministrasidesa/Umum/BukukeputusankepaladesaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Siode\Bukuadministrasidesa\Umum;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BukukeputusankepaladesaController extends Controller
+{
+    //
+}
