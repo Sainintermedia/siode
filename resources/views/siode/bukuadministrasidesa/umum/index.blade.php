f3d44371c9f0f78e3ff9bf32e6e2217c4a33fefd
nfz123 <akholidin0@gmail.com>
2023-02-21 20:34:50 +0700

    bukuinventarisdesa

--- resources/views/siode/bukuadministrasidesa/umum/index.blade.php
+++ resources/views/siode/bukuadministrasidesa/umum/index.blade.php
@@ -55,7 +55,7 @@
                             {{--  <i class="ion ion-bag"></i>  --}}
                             <i class="fa-solid fa-chart-simple"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{route('siode.buku.umum.bukuinventarisdesa.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <div class="col-md-12 col-12">
@@ -68,7 +68,7 @@
                             {{--  <i class="ion ion-bag"></i>  --}}
                             <i class="fa-solid fa-chart-simple"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{route('siode.buku.umum.bukuinventarisdesa.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <div class="col-md-12 col-12">
