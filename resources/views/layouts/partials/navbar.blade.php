3cd06a6be9c42373cc353faee392fa955e7b2fab
hiarief <arhistr14@gmail.com>
2023-05-12 20:23:18 +0700

    */revisi

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -10,24 +10,24 @@
             <ul class="nav col-12 col-lg-auto me-lg-auto justify-content-center mb-md-0 mb-2">
                 <li><a href="{{ route('home.index') }}" class="nav-link px-2 text-white">Home</a></li>
                 @auth
-                    @role('admin')
+                    {{--  @role('admin')
                         @can('users.index')
                             <li><a href="{{ route('users.index') }}" class="nav-link px-2 text-white">Users</a></li>
                         @endcan
                         <li><a href="{{ route('roles.index') }}" class="nav-link px-2 text-white">Roles</a></li>
                         <li><a href="{{ route('permissions.index') }}" class="nav-link px-2 text-white">Permissions</a></li>
-                    @endrole
+                    @endrole  --}}
                     <li><a href="{{ route('posts.index') }}" class="nav-link px-2 text-white">Posts</a></li>
                 @endauth
             </ul>
 
-            <form class="col-12 col-lg-auto mb-lg-0 me-lg-3 mb-3">
+            {{--  <form class="col-12 col-lg-auto mb-lg-0 me-lg-3 mb-3">
                 <input type="search" class="form-control form-control-dark" placeholder="Search..."
                     aria-label="Search">
-            </form>
+            </form>  --}}
 
             @auth
-                {{ auth()->user()->name }}&nbsp;
+                {{--  {{ auth()->user()->name }}&nbsp;  --}}
                 <div class="text-end">
                     <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2">Logout</a>
                     <a href="{{ route('siode.dashboard.index') }}" class="btn btn-outline-light me-2">Dashboard</a>
@@ -37,7 +37,7 @@
             @guest
                 <div class="text-end">
                     <a href="{{ route('login.perform') }}" class="btn btn-outline-light me-2">Login</a>
-                    <a href="{{ route('register.perform') }}" class="btn btn-warning">Sign-up</a>
+                    {{--  <a href="{{ route('register.perform') }}" class="btn btn-warning">Sign-up</a>  --}}
                 </div>
             @endguest
         </div>
