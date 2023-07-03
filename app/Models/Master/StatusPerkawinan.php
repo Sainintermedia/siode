277cf054d4e8a7801367eb4f7246f94278a1a64f
hiarief <arhistr14@gmail.com>
2023-07-03 08:13:37 +0700

    *?status-pernikahan

---
+++ app/Models/Master/StatusPerkawinan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Master;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StatusPerkawinan extends Model
+{
+    use HasFactory;
+
+    protected $table ='m_pernikahan';
+}
