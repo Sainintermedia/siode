a36dfe8d7deb30f1d4606adb5599252a0a5f2518
nfz123 <akholidin0@gmail.com>
2023-07-08 01:03:47 +0700

    penambahan statistic

--- app/Http/Controllers/Siode/Statistik/StatistikController.php
+++ app/Http/Controllers/Siode/Statistik/StatistikController.php
@@ -2,14 +2,40 @@
 
 namespace App\Http\Controllers\Siode\Statistik;
 
-use App\Http\Controllers\Controller;
+use App\Models\Master\RtRw;
 use Illuminate\Http\Request;
+use App\Models\Tabel\KartuKeluarga;
+use App\Http\Controllers\Controller;
+use App\Models\Tabel\KartuKeluargaAnggota;
+
+// use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
 
 class StatistikController extends Controller
 {
 
     public function index()
-    {   
-        return view('siode.statistik.index');
+    {
+        $kk = KartuKeluargaAnggota::where('sts_hub_kel', 1)->count();
+        $pen = KartuKeluargaAnggota::count();
+        $rt = KartuKeluarga::select('id', 'rt')->get()->groupBy('rt')->count();
+        $rw = KartuKeluarga::select('id', 'rw')->get()->groupBy('rw')->count();
+        $kp = KartuKeluarga::select('id', 'kp')->get()->groupBy('kp')->count();
+        $kkanggotalaki = KartuKeluargaAnggota::where('jenkel', '1')->count();
+        $kkanggotaperempuan = KartuKeluargaAnggota::where('jenkel', '2')->count();
+
+        // Prepare data for Pie Chart
+        $data = [
+            'Kartu Keluarga' => $kk,
+            'Penduduk' => $pen,
+            'RW' => $rw,
+            'RT' => $rt,
+            'Kampung' => $kp,
+            'KK Anggota Laki-laki' => $kkanggotalaki,
+            'KK Anggota Perempuan' => $kkanggotaperempuan,
+          
+        ];
+
+        return view('siode.statistik.index', compact('data'));
     }
 }
